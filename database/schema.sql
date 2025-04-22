931ef072538f2473bc521048b73d1586e69d50cd
Nada Zirari <zirarinada20@gmail.com>
2025-04-22 17:23:09 +0100

    ajouter table livre dans la bdd

--- database/schema.sql
+++ database/schema.sql
@@ -2,3 +2,11 @@ CREATE TABLE categories (
   id SERIAL PRIMARY KEY,
   nom VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE livres (
+  id SERIAL PRIMARY KEY,
+  titre VARCHAR(255) NOT NULL,
+  auteur VARCHAR(100),
+  statut VARCHAR(50), -- 'lu', 'en_cours', 'à_lire'
+  categorie_id INTEGER REFERENCES categories(id) ON DELETE SET NULL
+);
