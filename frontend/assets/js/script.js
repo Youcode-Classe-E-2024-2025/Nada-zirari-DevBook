2ad4611adc3af439a1a8d4d7cb06c673ca931d7f
Nada Zirari <zirarinada20@gmail.com>
2025-04-23 10:22:51 +0100

    ajouter un eventlistener pour le bouton submit pour recupererles infos du livre

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -3,3 +3,24 @@ const bookList = document.getElementById('bookList');
 const filter = document.getElementById('filtreCategorie'); 
 
 let books = [];
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const titre = document.getElementById('titre').value;
+    const auteur = document.getElementById('auteur').value;
+    const categorie = document.getElementById('categorie').value;
+  
+    const newBook = {
+      id: Date.now(),
+      titre,
+      auteur,
+      categorie,
+      statut: 'disponible'
+    };
+  
+    books.push(newBook);
+    form.reset();
+    renderBooks();
+  });
+  
