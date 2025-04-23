3709de8e30f8e8a9a05c2f75d1021a41a00d6648
Nada Zirari <zirarinada20@gmail.com>
2025-04-23 10:21:50 +0100

    declarer un tableau vide dans js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,3 +1,5 @@
 const form = document.getElementById('addBookForm');
 const bookList = document.getElementById('bookList');
 const filter = document.getElementById('filtreCategorie'); 
+
+let books = [];
